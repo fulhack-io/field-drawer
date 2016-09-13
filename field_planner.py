7c619d7afb405f311d4cb43b6257c71f9ce65978
klippo <klippo@deny.se>
2016-09-13 18:58:19 +0200

    Cleanup. Spelling.

--- field_planner.py
+++ field_planner.py
@@ -11,17 +11,18 @@ try:
         """Generate draw tools objects.
 
 
-        Anchor arguments needs to be comma-seperated and quoted. I.E. -a "59.866552,17.666234"
+        Anchor arguments needs to be comma-separated and quoted. I.E. -a "59.866552,17.666234"
 
-        Portal list file must contain comma-seperated lines.
+        Portal list file must contain comma-separated lines.
 
         """)
 
-    parser.add_argument('-a1', '--anchor_1', nargs='+', help="Coordinates to anchor. Must be comma-seperated", required=True)
-    parser.add_argument('-a2', '--anchor_2', nargs='+', help="Coordinates to first anchor. must be comma-seperated")
+    parser.add_argument('-a1', '--anchor_1', nargs='+', help="Coordinates to anchor. Must be comma-separated", required=True)
+    parser.add_argument('-a2', '--anchor_2', nargs='+', help="Coordinates to first anchor. must be comma-separated")
     parser.add_argument('-l', '--list', nargs='+', help="Path to file with comma-separated coordinates list.", required=True)
     args = parser.parse_args()
-except Exception:
+except Exception as e:
+    print(e)
     sys.exit(1)
 
 
@@ -30,44 +31,50 @@ if not args.anchor_2:
     anchor_2 = False
 else:
     anchor_2 = args.anchor_2[0].split(",")
-list = args.list[0]
-
-
+portal_list = args.list[0]
 
 
 class FieldDrawer:
 
-    def __init__(self, anchor_1, anchor_2, portal_list):
+    def __init__(self, a1, a2, portal_list):
         self.portal_list = portal_list
+        self.anchor_1 = a1
+        self.anchor_2 = a2
 
     def parse_portal_list(self):
-        with open(self.portal_list, "r") as portal_list:
-            portal_list = portal_list.readlines()
-            list_cnt = 0
-            list_len = len(portal_list)
 
-            print("["),
-            for row in portal_list:
+        try:
+            with open(self.portal_list, "r") as portal_list:
+                portal_list = portal_list.readlines()
+        except Exception as e:
+            print("Unable to read portal list.")
+            print(e)
 
-                if not anchor_2:
-                    print(TEMPLATE_LINK % (anchor_1[0],
-                                           anchor_1[1],
-                                           row.split(",")[-2].strip("\n"),
-                                           row.split(",")[-1].strip("\n")
-                                           )),
-                else:
-                    print(TEMPLATE_FIELD % (anchor_1[0],
-                                            anchor_1[1],
-                                            row.split(",")[-2].strip("\n"),
-                                            row.split(",")[-1].strip("\n"),
-                                            anchor_2[0],
-                                            anchor_2[1],
-                                            anchor_1[0],
-                                            anchor_1[1]
-                                           )),
-                if list_cnt != list_len -1:
-                    list_cnt += 1
-                    print(",")
+        list_cnt = 0
+        list_len = len(portal_list)
+
+        print("["),
+        for row in portal_list:
+            if not anchor_2:
+                print(TEMPLATE_LINK % (self.anchor_1[0],
+                                       self.anchor_1[1],
+                                       row.split(",")[-2].strip("\n"),
+                                       row.split(",")[-1].strip("\n")
+                                       )),
+            else:
+                print(TEMPLATE_FIELD % (self.anchor_1[0],
+                                        self.anchor_1[1],
+                                        row.split(",")[-2].strip("\n"),
+                                        row.split(",")[-1].strip("\n"),
+                                        self.anchor_2[0],
+                                        self.anchor_2[1],
+                                        self.anchor_1[0],
+                                        self.anchor_1[1]
+                                        )),
+            if list_cnt != list_len - 1:
+                list_cnt += 1
+                print(",")
+
         print("]")
 
     def generate(self):
@@ -76,7 +83,6 @@ class FieldDrawer:
 
 if __name__ == "__main__":
 
-    fd = FieldDrawer(anchor_1, anchor_2, list)
-
+    fd = FieldDrawer(anchor_1, anchor_2, portal_list)
     fd.generate()
 
